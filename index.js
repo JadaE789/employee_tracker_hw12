23b35c711313300b85b5361f25b2130e1a180df5
Jada Desormeaux <JadaE789@gmail.com>
2021-05-05 20:13:39 -0400

    started index.js

---
+++ index.js
@@ -0,0 +1,174 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+// const sql = require("./sql");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    firstPrompt();
+});
+
+// function which prompts the user for what action they should take
+function firstPrompt() {
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees by Department",
+          "View All Employees by Manager",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "Add Role",
+          "Remove Role",
+          "End"]
+      })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployee();
+            break;
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
+          // case "View Employees by Manager":
+          //   viewEmployeeByManager();
+          //   break;
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "Remove Employees":
+            removeEmployees();
+            break;
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+        // case "Remove Role":
+        //   removeRole();
+        //   break;
+
+        // case "Update Employee MAnager":
+        //   updateEmployeeManager();
+        //   break;
+
+        case "End":
+            connection.end();
+            break;
+        }
+      });
+}
+
+// View Employees
+
+function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    let query =
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+    ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+    ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      firstPrompt();
+    });
+    // console.log(query.sql);
+}
+
+// View Employees by Department
+
+function viewEmployeeByDepartment() {
+    console.log("Viewing employees by department\n");
+  
+    let query =
+    `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+    ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      // const departmentChoices = res.map(({ id, name }) => ({
+      //   name: `${id} ${name}`,
+      //   value: id
+      // }));
+  
+      const departmentChoices = res.map(data => ({
+        value: data.id, name: data.name
+      }));
+  
+      console.table(res);
+      console.log("Department view succeed!\n");
+  
+      promptDepartment(departmentChoices);
+    });
+    // console.log(query.sql);
+}
+
+// User choose the department list, then employees pop up
+
+function promptDepartment(departmentChoices) {
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Which department would you like to choose?",
+          choices: departmentChoices
+        }
+      ])
+      .then(function (answer) {
+        console.log("answer ", answer.departmentId);
+  
+        let query =
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+        FROM employee e
+        JOIN role r
+        ON e.role_id = r.id
+        JOIN department d
+        ON d.id = r.department_id
+        WHERE d.id = ?`
+  
+        connection.query(query, answer.departmentId, function (err, res) {
+          if (err) throw err;
+  
+          console.table("response ", res);
+          console.log(res.affectedRows + "Employees are viewed!\n");
+  
+          firstPrompt();
+        });
+      });
+}
+
